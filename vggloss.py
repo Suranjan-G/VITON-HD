cbe2e84aa4010cf5f465347c2294e55f722a2759
Suranjan-G <105277957+Suranjan-G@users.noreply.github.com>
2022-05-10 15:04:20 +0530

    Feature matching vgg loss

---
+++ vggloss.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from vgg import Vgg19
+
+class VGGLoss(nn.Module):
+    def __init__(self, gpu_ids):
+        super(VGGLoss, self).__init__()        
+        self.vgg = Vgg19().cuda()
+        self.criterion = nn.L1Loss()
+        self.weights = [1.0/32, 1.0/16, 1.0/8, 1.0/4, 1.0]        
+
+    def forward(self, x, y):              
+        x_vgg, y_vgg = self.vgg(x), self.vgg(y)
+        loss = 0
+        for i in range(len(x_vgg)):
+            loss += self.weights[i] * self.criterion(x_vgg[i], y_vgg[i].detach())        
+        return loss
